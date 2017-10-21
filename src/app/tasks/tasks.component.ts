e6b12a89258e7fb5143dd76bb03adc5fd84680de
Marcus Rocha <sif.marcusvinicius@gmail.com>
2017-10-21 17:32:32 -0300

    Criando listagem de tarefas

---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+const TASKS: Array<any> = [
+    {id: 1, title: 'Realizar entregas'},
+    {id: 2, title: 'Desenvolver componentes'},
+    {id: 3, title: 'Agendar reunião'},
+    {id: 4, title: 'Definir novos jobs'},
+    {id: 5, title: 'Implementar novo layout'},
+    {id: 6, title: 'Integrar novos modulos'},
+    {id: 7, title: 'Definir novos modulos'},
+];
+
+@Component({
+    selector: 'tasks',
+    templateUrl: './tasks.component.html'
+})
+
+export class TasksComponent implements OnInit{
+
+    public tasks;
+    public nomePagina;
+
+    public constructor(){
+        this.nomePagina = 'Listagem de Tarefas';
+    }
+
+    public ngOnInit(){
+        this.tasks = TASKS;
+    }
+}
