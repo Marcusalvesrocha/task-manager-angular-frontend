2afc45c37af0282ac6887a5a66cf5b719c301c47
Marcus Rocha <sif.marcusvinicius@gmail.com>
2017-10-21 15:22:06 -0300

    Adicionando o model Task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,25 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'APP DE TAREFAS';
+  
+  task: Task = {
+    id: 1,
+    title: 'Entregar encomendas dos clientes'
+  };
+
+  task2: Task = new Task(2, 'Receber novos projetos de desenvolvimento');
 }
+
+export class Task{
+  public id: number;
+  public title: string;
+
+  constructor(id: number, title: string){
+    this.id = id;
+    this.title = title
+  }
+}
+
