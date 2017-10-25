24f7f2a20738dc5c422e1c10d54b3c373e07ddd0
Marcus Rocha <sif.marcusvinicius@gmail.com>
2017-10-25 18:31:55 -0300

    Adicionando AppRoutingModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,34 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from "@angular/forms";
 import { HttpModule } from "@angular/http";
-import { RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { NavbarComponent } from "./navbar/navbar.component";
 import { TasksComponent } from "./tasks/tasks.component";
 import { TaskDetailComponent } from "./tasks/task-detail/task-detail.component";
+
 import { TaskService } from "./tasks/shared/task.service";
 
-const ROUTES = RouterModule.forRoot([
-  {
-    path: 'tasks/:id',
-    component: TaskDetailComponent
-  },
-  {
-    path: 'dashboard',
-    component: DashboardComponent
-  },
-  {
-    path: 'tasks',
-    component: TasksComponent
-  },
-  {
-    path: '',
-    redirectTo: '/dashboard',
-    pathMatch: 'full'
-  }
-]);
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
   declarations: [
@@ -40,10 +22,10 @@ const ROUTES = RouterModule.forRoot([
     TaskDetailComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     FormsModule,
-    HttpModule,
-    ROUTES
+    HttpModule
   ],
   providers: [ TaskService ],
   bootstrap: [AppComponent]
